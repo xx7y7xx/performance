da8efe6e7b607fc2b240e2f5ee95141a1397fb3d
Chen Yang <chenyang@masols.com>
2014-06-05 20:36:23 +0800

    stat impl

--- stat.py
+++ stat.py
@@ -1,47 +1,227 @@
-#!/bin/python2.7
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import sys
+import os
+from odf.opendocument import load
+from odf import text
 
-odf_dir = "."
-template = "template.ods"
+TS = 0.0
+XS = 0.0#xxdebug
+DEBUG = 0
 
-#
-# functions
-#
+#cfg.py - https://docs.google.com/a/masols.com/document/d/1ge9HjJJ6Rfb-QjLZrZM5pOwWoWjZ5Wf-MgH0jqtMHwU/edit
+from cfg import QIAN
+from cfg import REVIEW
 
-def getdate():
-    return False
+def valuetype(val):
+    valuetype="string"
+    if isinstance(val,str): valuetype="string"
+    if isinstance(val,int): valuetype="float"
+    if isinstance(val,float): valuetype="float"
+    if isinstance(val,bool): valuetype="boolean"
+    return valuetype
 
-def copy(src, dst):
-    return False
+def get_name(fn) :
+    return fn.replace(".odt", "")
 
-def create_new_odf():
-    date = getdate()
-    copy(template, str(date) + ".ods")
-    return False
+def get_num(para) :
+    num = ""
+    for child in para.childNodes :
+        #pt=Para Text
+        #<p>$text<p>
+        if child.nodeType is child.TEXT_NODE :
+            pt = child.data
+            pt = pt.encode("utf8")
+            print "[get_num] pt="+pt
+            print "[get_num] type(pt)="+str(type(pt))
+            pt = filter(str.isdigit, pt)
+            if pt != "" :
+                num += pt
+        #pst=Para Span Text
+        #<p>
+        #  <span>$text</span>
+        #  <span>$text</span>
+        #</p>
+        if child.nodeType is child.ELEMENT_NODE : 
+            for chd in child.childNodes :
+                if chd.nodeType is chd.TEXT_NODE :
+                    pst = chd.data
+                    pst = pst.encode("utf8")
+                    print "[get_num] pst="+pst
+                    print "[get_num] type(pst)="+str(type(pst))
+                    pst = filter(str.isdigit, pst)
+                    if pst != "" : 
+                        num += pst
+    print "[get_num] num='"+num+"'"
+    if num == "" :
+        return "0"
+    else :
+        return num
 
-def read_odf(file):
-    return False
+def get_total_money() :
+    global QIAN
+    global XS
+    for n,m in QIAN.items() :
+        print "[get_total_money] n='"+n+"'"
+        print "[get_total_money] m='"+str(m)+"'"
+        XS += m
 
-def insert_row(odf, data):
-    return False
+def cell(tr, val) :
+    print "[cell] type="+str(type(val))
+    tc = TableCell(valuetype=valuetype(val), value=str(val))
+    tr.addElement(tc)
+    p = P(stylename=tablecontents,text=str(val))
+    tc.addElement(p)
 
-def sort(odf):
-    return False
+def single_odt(path, uname, create) :
+    global TS
+    global XS
+    global DEBUG
+    global REVIEW
 
+    print "[single_odt] path="+path
+    doc = load(path)
+    
+    if create == 1 :
+        # new row
+        tr = TableRow()
+        table.addElement(tr)
+        # name column
+        cell(tr, uname)
+    
+    contrib = []
 
-#
-# start
-#
+    idx = 0
+    for para in doc.getElementsByType(text.P):
+        
+        if idx >= 8 :
+            break
+        
+        # no child
+        if not para.hasChildNodes() :
+            continue
+        
+        num = get_num(para)
+        print "[single_odt] num='"+num+"'"
+        
+        if create == 1 :
+            #table
+            cell(tr, int(num))
+        
+        # store contrib
+        contrib.append(int(num))
+        
+        idx += 1
+    
+    # every single contrib
+    self_ticket = contrib[0]
+    other_ticket = contrib[1]
+    wiki = contrib[2]
+    wiki_code = contrib[3]
+    daima = contrib[4]
+    bug = contrib[5]
+    xuqiu = contrib[6]
+    zuyuan = contrib[7]
+    
+    all_as_code = float(daima + xuqiu*10 + bug*20 + wiki_code/100 + wiki/20 + self_ticket/50 + other_ticket/40 + zuyuan/10)
+    print "all_as_code="+str(all_as_code)
+    
+    if create == 0 :
+        TS += all_as_code
+    
+    if create == 1 :
+        print "QIAN[uname]="+str(QIAN[uname])
+        print "XS="+str(XS)
+        print "TS="+str(TS)
 
-def main(argv):
-    big_table = create_new_odf()
+        score = (all_as_code / TS) / (QIAN[uname] / XS)
 
-    for odf in odf_dir:
-        data = read_odf(odf)
-        insert_row(big_table, data)
+        cell(tr, all_as_code)               # all as score
+        cell(tr, (all_as_code / TS))        # code score
+        if DEBUG != 0 :
+            cell(tr, QIAN[uname])           # money
+            cell(tr, (QIAN[uname] / XS))    # money score
+        cell(tr, score)
+    
+def all_odt(table, create) :
+    for root, dirs, files in os.walk( REVIEW ):
+        for fn in files:
+            single_odt(root+"/"+fn, get_name(fn), create)
 
-    sort(big_table)
+def header(table) :
+    global DEBUG
 
-if __name__ == "__main__":
-    main(sys.argv)
+    tr = TableRow()
+    table.addElement(tr)
+    
+    cell(tr, "name")
+    
+    cell(tr, "self ticket")
+    cell(tr, "other ticket")
+    cell(tr, "wiki")
+    cell(tr, "wiki code")
+    cell(tr, "code")
+    cell(tr, "bug ticket")
+    cell(tr, "3m")
+    cell(tr, "team member")
+    
+    cell(tr, "all as code")
+    cell(tr, "code score")
+    if DEBUG != 0 :
+        cell(tr, "money")
+        cell(tr, "money score")
+    cell(tr, "score")
+    cell(tr, "ranking")
+    
+def footer(table) :
+    tr = TableRow()
+    table.addElement(tr)
+    
+    cell(tr, "zhuguan")
+    cell(tr, "")
+    
+    cell(tr, "zongjingli")
+    cell(tr, "")
+    
+
+# main
+
+from odf.opendocument import OpenDocumentSpreadsheet
+from odf.style import Style, TextProperties, ParagraphProperties
+from odf.style import TableColumnProperties
+from odf.text import P
+from odf.table import Table, TableColumn, TableRow, TableCell
+
+doc = OpenDocumentSpreadsheet()
+# Create a style for the table content. One we can modify
+# later in the word processor.
+tablecontents = Style(name="Table Contents", family="paragraph")
+tablecontents.addElement(ParagraphProperties(numberlines="false", linenumber="0"))
+doc.styles.addElement(tablecontents)
+
+# Create automatic styles for the column widths.
+# We want two different widths, one in inches, the other one in metric.
+# ODF Standard section 15.9.1
+widthshort = Style(name="Wshort", family="table-column")
+widthshort.addElement(TableColumnProperties(columnwidth="1.7cm"))
+doc.automaticstyles.addElement(widthshort)
+
+widthwide = Style(name="Wwide", family="table-column")
+widthwide.addElement(TableColumnProperties(columnwidth="1.5in"))
+doc.automaticstyles.addElement(widthwide)
+
+# Start the table, and describe the columns
+table = Table(name="jixiao")
+table.addElement(TableColumn(numbercolumnsrepeated=4,stylename=widthshort))
+table.addElement(TableColumn(numbercolumnsrepeated=3,stylename=widthwide))
+
+# glue start
+get_total_money()
+header(table)
+all_odt(table, 0)
+all_odt(table, 1)
+footer(table)
+# glue end
+
+doc.spreadsheet.addElement(table)
+doc.save("jixiao", True)
