9160656e2e3ed891409d18733663b36579bd45df
chenyang <chenyang@localhost.localdomain>
2014-09-05 18:05:15 +0800

    Notify every developer at end of every month.

---
+++ notify.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+IP="192.168.1.153"
+ULIST="userlist.txt"
+DATE="`date +'%Y年%m月份'`"
+
+# create random dir for me
+JDIR="/tmp/jixiao_$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
+mkdir -p "$JDIR"
+cd "$JDIR"
+
+# load useful functions
+wget https://raw.githubusercontent.com/sp-chenyang/xxutils/master/xxutils.sh \
+    && chmod a+x xxutils.sh \
+    && . xxutils.sh
+
+# Get user list from trac
+# gen userlist.txt
+getusers
+
+# create ticket for each user.
+while read -r username;
+do
+    cmd='sudo python /home/chenyang/tool/newticket.py"'
+    cmd="$cmd --reporter ci"
+    cmd="$cmd --owner \"$username\""
+    cmd="$cmd --type review"
+    cmd="$cmd --summary \"${DATE}绩效统计 - 每月自评表\""
+    cmd="$cmd --description \"提交本月自评表\""
+    echo $cmd
+    rcmd "$IP" "$cmd"
+done < $ULIST
+
+#
+# send mail notification
+#
+
+SUBJECT="${DATE}绩效统计"
+MSG="请将自己的绩效统计表格扔到\nsmb://192.168.2.21/share/Review/`date +'%Y-%m'`"
+
+xsendmail "$SUBJECT" "$MSG" "glue@spolo.org"
