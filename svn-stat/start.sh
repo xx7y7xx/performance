a4df43b8d11c3d7dee05de6c4cea1e9841e02ef0
Devin Chen <d3vin.chen@gmail.com>
2015-11-04 19:47:18 +0800

    Exclude dirs for wware document project.

--- svn-stat/start.sh
+++ svn-stat/start.sh
@@ -12,36 +12,71 @@ OUTPUT="${WORKSPACE}/output"
 STATSVN_ARG="-verbose -charset utf-8 -disable-twitter-button -username ci -password sp12345678"
 SVNPARAM=" --username=ci --password=sp12345678 --no-auth-cache "
 
+# Contains exclude cfg file for each repo.
+ExcludeDirsCfgDir="${WORKSPACE}/svn-stat/exclude_dirs"
+
+function getExcludeDirs() {
+}
+
 # one repo at a time
-## $1 modual name
-stat_repo()
-{
-	local sr_repo_name="$1"
-	
-	echo "Processing [${sr_repo_name}] ... "
-	
-	local sr_svn_dir="${SVNDIR}/${sr_repo_name}"
-	local sr_svn_log="${SVNDIR}/${sr_repo_name}.log"
-	local sr_statsvn_log="${SVNDIR}/${sr_repo_name}.statsvn.log"
-	local sr_cache_dir="${SVNDIR}/${sr_repo_name}_cache"
-	local sr_output_dir="${OUTPUT}/${sr_repo_name}"
-	
-  # Do a fresh checkout or update exist one.
-  if [ -d $sr_svn_dir ]; then
-    svn update $SVNPARAM $sr_svn_dir
+#   $1 - modual name
+#   $2 - (Optional) path in this($1) modual.
+#        When set, processing only the selected($2) path.
+stat_repo() {
+  local sr_repo_name="$1"
+  local sr_repo_path="$2"
+  
+  echo "Processing [${sr_repo_name} : ${sr_repo_path}] ... "
+  
+  if [ -z $sr_repo_path ]; then
+    local sr_svn_url="http://glue.spolo.org/svn/$sr_repo_name"
+    local sr_repo_encode_name=$sr_repo_name
   else
-    svn checkout $SVNPARAM http://glue.spolo.org/svn/$sr_repo_name $sr_svn_dir
+    local sr_svn_url="http://glue.spolo.org/svn/$sr_repo_name/$sr_repo_path"
+    # trunk/wware/tech.com -> trunk_wware_tech.com
+    local sr_repo_encode_name="${sr_repo_name}_`echo $sr_repo_path | sed -e 's/\//_/g'`"
   fi
 
-	# create svn log, xml format
-	svn log $SVNPARAM -v --xml ${sr_svn_dir} > ${sr_svn_log}
-    echo "status code of svn log is : [$?]"
-	mkdir -p ${sr_output_dir}
-	
-	# prepare arguments, and call statsvn.
-	local sr_statsvn_arg="${sr_svn_log} ${sr_svn_dir} -cache-dir ${sr_cache_dir} -output-dir ${sr_output_dir} -title ${sr_repo_name} ${STATSVN_ARG}"
-	java -jar ${STATSVN} ${sr_statsvn_arg} &> ${sr_statsvn_log}
-    echo "status code of statscn.jar : [$?]"
+  local sr_svn_dir="${SVNDIR}/${sr_repo_encode_name}"
+  local sr_svn_log="${SVNDIR}/${sr_repo_encode_name}.log"
+  local sr_cache_dir="${SVNDIR}/${sr_repo_encode_name}_cache"
+  local sr_output_dir="${OUTPUT}/${sr_repo_encode_name}"
+
+  # Do a fresh checkout or update exist one.
+  if [ -d $sr_svn_dir/.svn ]; then
+    svn update $SVNPARAM $sr_svn_dir
+  else
+    svn checkout $SVNPARAM $sr_svn_url $sr_svn_dir
+  fi
+  
+  # create svn log, xml format
+  svn log $SVNPARAM -v --xml ${sr_svn_dir} > ${sr_svn_log}
+  local ret=$?
+  if [ "X$ret" != "X0" ]; then
+    echo "status code of svn log is [${ret}]."
+    exit $ret
+  fi
+  mkdir -p ${sr_output_dir}
+ 
+  # Exclude dir for statsvn
+  exclude_dir=""
+  for line in `cat ${ExcludeDirsCfgDir}/${sr_repo_encode_name}.txt`; do
+    if [ ! -z $exclude_dir ]; then
+      exclude_dir+=";"
+    fi
+    exclude_dir+="$line/**"
+  done
+  exclude_dir=" -exclude ${exclude_dir} "
+  echo "[Debug] exclude dirs : " ${exclude_dir}
+ 
+  # prepare arguments, and call statsvn.
+  local sr_statsvn_arg="${sr_svn_log} ${sr_svn_dir} -cache-dir ${sr_cache_dir} -output-dir ${sr_output_dir} -title ${sr_repo_encode_name} ${exclude_dir} ${STATSVN_ARG}"
+  java -jar ${STATSVN} ${sr_statsvn_arg}
+  local ret=$?
+  if [ "X$ret" != "X0" ]; then
+    echo "status code of statsvn is [${ret}]."
+    exit $ret
+  fi
 }
 
 #
@@ -69,6 +104,8 @@ date
 #stat_repo "tooltpl"
 #stat_repo "cff"
 stat_repo "glue"
+stat_repo "glue" "trunk/wware/wide.com"
+stat_repo "glue" "trunk/wware/tech.com"
 #stat_repo "spp_sdk_1010m"
 #stat_repo "app_project"
 
